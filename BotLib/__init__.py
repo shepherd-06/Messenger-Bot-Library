da1d44f30dee0d8e4decb3078500dfb16a1c2773
Ibtehaz Shawon <ibtehaz.shawon@gmail.com>
2019-01-28 15:54:55 +0600

    initial commit

---
+++ BotLib/__init__.py
@@ -0,0 +1,2 @@
+def create_app():
+    print("Boom Boom!")
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+install_requires = [
+    
+]
+
+setup(name='bizarro',  # alternative name docket
+      packages = ['BotLib'],
+      version='0.0.1.dev1',
+      description='',
+      url='https://github.com/ibtehaz-shawon/bizarro',
+      author='Ibtehaz Shawon',
+      author_email='ibtehaz.92@gmail.com',
+      license='MIT',
+      install_requires=[
+            install_requires
+      ],
+      entry_points={
+        'console_scripts': [
+            'bizarro = BotLib.__init__:create_app'
+        ]
+        },
+       classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+      zip_safe=False)
