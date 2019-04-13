11789a6a913fb2b3ccfb2a92c45305394c887675
ibtehaz-shawon <10481417+ibtehaz-shawon@users.noreply.github.com>
2019-04-13 14:50:40 +0600

    Fking indentation on the doc string

--- bizarro/buttons/button_generator.py
+++ bizarro/buttons/button_generator.py
@@ -7,10 +7,10 @@ class Button(MotherClass):
     """
     Button class Generates multiple types of button to use in different templates and replies for Messenger Bot Platform
     Currently, the supported button types are
-    * Url Button
-    * Postback Button
-    * Call Button
-    * *Share Button* (This might be little buggy! I have not been able to run a full test on it yet)
+        * Url Button
+        * Postback Button
+        * Call Button
+        * *Share Button* (This might be little buggy! I have not been able to run a full test on it yet)
 
     Please check Facebook's documentation for more information. See the **Reference** url on top of every buttons.
     """
