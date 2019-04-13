cd426fbd9ebffc9ac31e0d455a84c9a0505308b3
ibtehaz-shawon <10481417+ibtehaz-shawon@users.noreply.github.com>
2019-04-13 14:45:08 +0600

    Nightwish - wish I had an angel

--- bizarro/buttons/button_generator.py
+++ bizarro/buttons/button_generator.py
@@ -2,19 +2,18 @@ from bizarro.utility.tag import Tags
 from bizarro.utility.mother import MotherClass
 from bizarro.buttons.button_validator import ButtonValidation
 
-"""
-Button class Generates multiple types of button to use in different templates and replies for Messenger Bot Platform
-Currently, the supported button types are
+
+class Button(MotherClass):
+    """
+    Button class Generates multiple types of button to use in different templates and replies for Messenger Bot Platform
+    Currently, the supported button types are
     * Url Button
     * Postback Button
     * Call Button
     * *Share Button* (This might be little buggy! I have not been able to run a full test on it yet)
 
-Please check Facebook's documentation for more information. See the **Reference** url on top of every buttons.
-"""
-
-
-class Button(MotherClass):
+    Please check Facebook's documentation for more information. See the **Reference** url on top of every buttons.
+    """
 
     def __init__(self):
         super().__init__()
