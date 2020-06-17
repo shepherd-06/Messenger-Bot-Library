fa5eddcee0000f547b421af155f73529b7a0d2d3
p1r-a-t3 <10481417+p1r-a-t3@users.noreply.github.com>
2020-06-18 02:00:05 +0600

    pyfiglet is needed. Without it thelibrary does not work

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from os import path
 from setuptools import setup
 
 install_requires = [
-    'phonenumbers'
+    'phonenumbers',
+    'pyfiglet',
 ]
 
 this_directory = path.abspath(path.dirname(__file__))
@@ -12,7 +13,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(name='messenger_bot_library',  # alternative name docket
       packages=['messenger_bot', 'messenger_bot.utility',
                 'messenger_bot.template', 'messenger_bot.buttons'],
-      version='2.0.2.beta',
+      version='2.0.3.beta',
       description='A python package that helps you to create payload for facebook messenger bot.',
       long_description=long_description,
       long_description_content_type='text/markdown',
